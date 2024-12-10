bde1325320906eb3faf19309f81c5a6ebf6df5f9
Paulina Rosales <prosales@csuchico.edu>
2024-12-10 13:24:52 -0800

    Removed unused imports in rectanglePage.py

--- src/rectanglePage.py
+++ src/rectanglePage.py
@@ -1,11 +1,4 @@
-import os
-import sys
 from email.quoprimime import header_decode
-from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.label import Label
-from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.core.window import Window
 from kivymd.uix.toolbar import MDTopAppBar
